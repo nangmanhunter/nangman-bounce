17b0448b4f9480a1695a16826adb63c15eadcdac
djwlfdls1 <djwlfdls1@gmail.com>
2026-06-26 20:12:30 +0900

    add: index nav-centered

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,9 +1,20 @@
+<script setup>
+const items = [
+  { label: 'ascii', icon: 'i-heroicons-calculator', to: '/ascii' },
+  { label: 'svgtopng', icon: 'i-heroicons-megaphone', to: '/svgtopng' }
+  // { label: '공지사항', icon: 'i-heroicons-megaphone', to: '/notice' },
+  // { label: '공지사항', icon: 'i-heroicons-megaphone', to: '/notice' }
+]
+</script>
+
 <template>
-  <p
-    style="text-align-last: justify;"
-  >
-    동해물과 백두산이 마르고 닳도록 하느님이 보우하
-    <br>
-    dsdd
-  </p>
+  <div class="flex justify-center items-center h-screen">
+    <div class="max-w-xs mx-auto">
+      <UNavigationMenu
+        orientation="vertical"
+        :items="items"
+        class="w-full"
+      />
+    </div>
+  </div>
 </template>
