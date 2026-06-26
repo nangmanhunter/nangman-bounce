d1cfd88e829c19d39ffb2dd12f566a599a618d4b
djwlfdls1 <djwlfdls1@gmail.com>
2026-06-26 15:40:54 +0900

    ref: hero description

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,63 +1,51 @@
 <template>
   <div>
-  <UPageHero
-    title="ASCII Converter"
-    description="A Nuxt/Vue-powered creative studio that transforms images and plain text into beautiful, retro-style ASCII art with modern performance."
-    headline="New release"
-     orientation="horizontal"
-  >
-
-  <div>
-      <img
-    src="/favicon.ico"
-    class="w-10 h-10"
-  >
-<UPageCard
-      title="ascii-path"
-      description="Convert Paths to Ascii"
-      icon="i-lucide-spline" 
-      to="./pngtoascii"
+    <UPageHero
+      title="ASCII Converter"
+      description="A image-based creative studio that transforms images and plain text into beautiful, retro-style ASCII art with modern performance."
+      headline="New release"
       orientation="horizontal"
-      spotlight
-      spotlight-color="primary"
-      _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]" 
     >
-      </UPageCard>
+      <div>
+        <img
+          src="/favicon.ico"
+          class="w-10 h-10"
+        >
+        <UPageCard
+          title="ascii-path"
+          description="Convert Paths to Ascii"
+          icon="i-lucide-spline"
+          to="./pngtoascii"
+          orientation="horizontal"
+          spotlight
+          spotlight-color="primary"
+          _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]"
+        />
 
-    <UPageCard
-      title="ascii-shape"
-      description="Convert Shapes to Ascii"
-      icon="i-lucide-shapes" 
-      to="./pngtoascii/shape"
-      orientation="horizontal"
-      spotlight
-      spotlight-color="primary"
-      _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]" 
-    >
-      </UPageCard>
+        <UPageCard
+          title="ascii-shape"
+          description="Convert Shapes to Ascii"
+          icon="i-lucide-shapes"
+          to="./pngtoascii/shape"
+          orientation="horizontal"
+          spotlight
+          spotlight-color="primary"
+          _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]"
+        />
 
-    <UPageCard
-      title="ascii-bigtext"
-      description="Convert Texts to Ascii"
-      icon="i-lucide-type" 
-      to="./pngtoascii/bigtext"
-      orientation="horizontal"
-      spotlight
-      spotlight-color="primary"
-      _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]" 
-      highlight
-      highlight-color="primary"
-    >
-      </UPageCard>
-
-  </div>
-
-</UPageHero>
-
-
-
-
-
-
+        <UPageCard
+          title="ascii-bigtext"
+          description="Convert Texts to Ascii"
+          icon="i-lucide-type"
+          to="./pngtoascii/bigtext"
+          orientation="horizontal"
+          spotlight
+          spotlight-color="primary"
+          _class="[--spotlight-color:var(--ui-error)] [--spotlight-size:200px]"
+          highlight
+          highlight-color="primary"
+        />
+      </div>
+    </UPageHero>
   </div>
 </template>
--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,17 +1,7 @@
 <template>
-  <div class="w-full max-w-md p-4">
-    <label class="flex items-center justify-between w-full p-2 border border-gray-300 bg-red-100 rounded-lg cursor-pointer hover:bg-gray-700">
-      
-      <span class="px-4 py-1.5 bg-slate-800 text-white text-sm rounded-md font-medium">
-        파일 선택
-      </span>
-
-      <input 
-        type="file" 
-        accept="image/png" 
-        class="text-sm text-gray-500 w-40 file:hidden"
-      >
-      
-    </label>
-  </div>
+  <p style="text-align-last: justify;">
+    동해물과 백두산이 마르고 닳도록 하느님이 보우하
+    <br>
+    dsdd
+  </p>
 </template>
